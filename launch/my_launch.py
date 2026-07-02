cfc8f312481d2c566705f6eac31e8b1fe86cdd7c
Eyasuder <eyasuderbew2008@gmail.com>
2026-07-02 13:01:48 +0900

    rename Xacro files for consistency

--- launch/my_launch.py
+++ launch/my_launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('my_robot'))
-    xacro_file = os.path.join(pkg_path, 'robot_urdf_description', 'robot.urdf.xacro')
+    xacro_file = os.path.join(pkg_path, 'robot_urdf_description', 'robot_urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
    
    
